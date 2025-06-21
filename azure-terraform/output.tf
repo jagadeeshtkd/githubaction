2e021559ff74b983ba4b274d125f71b408d2c52d
jagadeeshtkd <jagadeesh1998a@gmail.com>
2025-06-21 22:29:00 +0530

    output modified with 2 index

--- azure-terraform/output.tf
+++ azure-terraform/output.tf
@@ -2,10 +2,10 @@ output "vnet_name" {
   value = azurerm_virtual_network.spoke_vnet.name
 }
 
-output "vm_name" {
-  value = azurerm_linux_virtual_machine.vm.name
+output "vm_name_1" {
+  value = azurerm_linux_virtual_machine.vm[0].name
 }
 
-output "vm_name" {
-  value = azurerm_linux_virtual_machine.vm.name
+output "vm_name_2" {
+  value = azurerm_linux_virtual_machine.vm[1].name
 }
